8bd0c0507a9a3909a63816b43473d5559f6aec88
kamilfatkulin3@gmail.com <kamilfatkulin3@gmail.com>
2026-08-16 19:17:35 +0500

    chore: add API constants template file

---
+++ Health/Services/ApiConstans.template.cs
@@ -0,0 +1,10 @@
+﻿using Google.GenAI.Types;
+
+namespace Health.Services
+{
+    public static  class ApiConstans
+    {
+        //Insert your API key here
+        public const string ApiKey = "YOUR_API_KEY_HERE";
+    }
+}
